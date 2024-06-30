9e84eabe170748e4c62dbbe453d108f391da30e6
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-06-30 08:06:12 +0100

    Path & Query Params url

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ app = FastAPI()
 #app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 app = FastAPI(swagger_ui_parametres={"syntaxHighlight.theme": "obsidian"})
 
-@app.get("/")
-async def root():
-    return {"message": "3assléma"}
+# @app.get("/")
+# async def root():
+#     return {"message": "3assléma"}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
+
+# @app.get("/items/")
+# async def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
