5d88ec19addc926e1c108a4cc7df6ae6babf38f3
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-06-30 07:49:53 +0100

    change theme swagger to obsidian

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
+#app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
+app = FastAPI(swagger_ui_parametres={"syntaxHighlight.theme": "obsidian"})
 
 @app.get("/")
 async def root():
