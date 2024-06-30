5df9b6fc4e0a06c9f193d954fe6028f1f1935b0e
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-06-30 08:37:34 +0100

    Pydantic Schemas & data validation

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+########################################################################################################
 from fastapi import FastAPI, Query, Path
-# from pydantic import BaseModel
+from pydantic import BaseModel
 
+########################################################################################################
 app = FastAPI()
 #app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 app = FastAPI(swagger_ui_parametres={"syntaxHighlight.theme": "obsidian"})
-
+########################################################################################################
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 # class Item(BaseModel):
 #     name: str
 #     price: float
 #     is_offer: bool = None
+######################################################################################################
+@app.post("/items/")
+async def create_item(item: Item):
+    # print("Validation data:", item.model_dump())
+    # return {"message": "Item created successfully", "data": item.model_dump()}
+    print("Validation data:", item.dict())
+    return {"message": "Item created successfully", "data": item.dict()}
 
 # @app.get("/")
 # async def root():
@@ -34,6 +48,7 @@ app = FastAPI(swagger_ui_parametres={"syntaxHighlight.theme": "obsidian"})
 # async def read_items(q: str = Query(None, min_length=3, max_length=50)):
 #     return {"q": q}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int = Path(..., title="The ID of the item to get", ge=1)):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int = Path(..., title="The ID of the item to get", ge=1)):
+#     return {"item_id": item_id}
+##########################################################################################################
